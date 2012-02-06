5b1a60a2d2b0baaff9a7e0cb503350b8b45c35c1
Jon Doveston <jon@llamadigital.net>
2012-02-06 14:21:39 +0000

    removed bin, and added an optional proxy

--- lib/pan_handler/configuration.rb
+++ lib/pan_handler/configuration.rb
@@ -12,7 +12,17 @@ class PanHandler
     end
 
     def pandoc
-      @pandoc ||= (defined?(Bundler::GemfileError) ? `bundle exec which pandoc-proxy` : `which pandoc-proxy`).chomp
+      @pandoc ||= [which_pandoc_proxy, which_pandoc].find{ |c| not c.empty? }
+    end
+
+    private
+
+    def which_pandoc
+      (defined?(Bundler::GemfileError) ? `bundle exec which pandoc` : `which pandoc`).chomp
+    end
+
+    def which_pandoc_proxy
+      (defined?(Bundler::GemfileError) ? `bundle exec which pandoc-proxy` : `which pandoc-proxy`).chomp
     end
   end
 
